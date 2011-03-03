1a48538d569acb8de9b4c7f4725234d4a466d6c3
William Rowan <wmrowan@wmrowan-thinkpad.(none)>
2011-03-02 19:10:29 -0800

    tried to set getter on gl object

--- static/webglib.js
+++ static/webglib.js
@@ -69,3 +69,48 @@ var TextureDelegate = function(texture_paths) {
     return tex_map[path] && tex_map[path].loaded && tex_map[path].image
   }
 }
+
+function setupWebGL(canvas) {
+  var canvas = document.getElementById('gl_canvas');
+  var gl = WebGLUtils.setupWebGL(canvas);
+	var gl_err;
+	for(var attrib in gl) {
+    gl_err.__defineGetter__(attrib, function() {
+      return gl[attrib];
+		});
+	}
+	return gl_err;
+}
+
+function loadShader(gl, sname) {
+  var shaderScript = document.getElementById(sname);
+	if(!shaderScript) return null;
+	var shader;
+	if(shaderScript.type === "x-shader/x-fragment") {
+	  shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if(shaderScript.type === "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    return null;
+	}
+  gl.shaderSource(shader, shaderScript.text);
+	gl.compileShader(shader);
+	if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert("Shader did not compile: " + gl.getShaderInfoLog(shader));
+		return null;
+	}
+  return shader;
+}
+
+function loadShaderProgram(gl, pname) {
+  var fragmentShader = loadShader(gl, pname + ".frag.glsl");
+	var vertexShader = loadShader(gl, pname + ".vert.glsl");
+	var program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    alert("Shader program fail");
+	}
+	return program;
+}
