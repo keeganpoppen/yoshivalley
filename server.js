8f1a68f6055f98767eca7ee1f0211e0fee46c20a
Keegan Poppen <keegan.poppen@gmail.com>
2011-03-06 03:48:17 -0800

    added some message infrastructure and junk

--- server.js
+++ server.js
@@ -42,11 +42,72 @@ util.log('server listening on port 6969... hotly')
 
 var clients = {};
 
+/*
+ * { 
+ *  player_id: {
+ *      obj: socketio_obj,
+ *      TODO: ADD MORE
+ * }
+ */
+
+var players = {};
+var viewers = {};
+
+function broadcast_to_viewers(msg) {
+    for(var id in viewers) {
+        viewers[id].send(msg)
+    }
+}
+
 var socket = io.listen(server)
 
 socket.on('connection', function(client) {
-    if(client in clients) util.log('WWTF!!!!!')
+    //if(client in clients) util.log('WWTF!!!!!')
+    
+    //the first message that comes from the client needs to determine
+    //the 'client_type'-- either 'player' or 'viewer'
+    client.once('message', function(message) { 
+        if(message.client_type == 'player') {
+            util.log('hesa playa')
+            players[client.sessionId] = client
 
+            //send the player his/her player_id
+            client.send({'type': 'init:setid', 'player_id': client.sessionId})
+
+            broadcast_to_viewers({'type': 'player:add', 'player_id': client.sessionId})
+
+            client.on('message', function(message) {
+                message.player_id = client.sessionId
+                broadcast_to_viewers(message)
+            })
+
+            client.on('disconnect', function(message) {
+                util.log('player with id: ' + client.sessionId + ' disconnected!')
+                delete players[client.sessionId]
+            })
+        } else if(message.client_type == 'viewer') {
+            util.log('new viewer connected')
+            viewers[client.sessionId] = client
+
+            //if players are connected before the viewer, just add them now
+            for(var id in players) {
+                client.send({'type': 'player:add', 'player_id': id})    
+            }
+
+            client.on('message', function(message) {
+                //TODO: don't actually know if anything should be happening here right now
+            })
+
+            client.on('disconnect', function() {
+                util.log('a viewer disconnected!')
+                delete viewers[client.sessionId]
+            })
+        } else {
+            util.log('player with unknown / undefined player type tried to connect')
+        }
+    })
+
+    /*
     clients[client.sessionId] = {'obj': client, 'start': Date.now(), 'num_msg': 0}
     client.on('message', function(msg) {
         if(msg.type == 'accel') {
@@ -63,6 +124,7 @@ socket.on('connection', function(client) {
         delete clients[client]
         util.log('client disconnected! what a douche!')
     })
+    */
 })
 
 var print_rates = function() {
