e52adf31a52041d1467e221254409bbbe17895a2
William Rowan <wmrowan@wmrowan-thinkpad.(none)>
2011-03-05 16:57:22 -0800

    got a tetrahedron showing up. Three sucks

--- static/shapes.js
+++ static/shapes.js
@@ -8,51 +8,79 @@
  *  String bumpTex;
  */
 
+function makeTetrahedronMesh() {
+    var verts = [];
+    var indices = [];
+
+    verts.push(-1.0,-1.0,0.0);
+    verts.push(1.0,-1.0,0.0);
+    verts.push(0.0,1.0,0.0);
+    verts.push(0.0,0.0,2.0);
+
+    indices.push(0,1,2);
+    indices.push(0,1,3);
+    indices.push(0,2,3);
+    indices.push(1,2,3);
+
+    var mesh = {};
+    mesh.vertices = new Float32Array(verts);
+    mesh.indices = new Uint16Array(indices);
+
+    return mesh;
+}
+
 function makeSphereMesh(radius) {
-  var step = 10.0 * Math.PI / 180.0; //5 degrees at a time
-  var verts_per_ring = 5.0 * Math.PI / step;
+    var step = 10.0 * Math.PI / 180.0; //5 degrees at a time
+    var verts_per_ring = 5.0 * Math.PI / step;
 
-  var verts = [];
-  var indices = [];
-  verts.push(0,-radius,0);
-  
-  //Start with first cap
-  var xzRadius = radius*Math.sin(step);
-  for(var lon = step; lon < 2*Math.PI; lon += step) {
-    var len = verts.push(xzRadius*Math.sin(lon), radius*Math.cos(step), xzRadius*Math.cos(lon));
-    indices.push(0, len-2, len-1);
-  }
-  //Fill in hole at boundary
-  indices.push(0, 1, verts_per_ring);
+    var verts = [];
+    var indices = [];
+    verts.push(0,-radius,0);
 
-  //All stripes in the middle
-  for(var lat = 2*step; lat < Math.PI-step; lat += step) {
-    var xzRadius = radius*Math.sin(lat);
+    //Start with first cap
+    var xzRadius = radius*Math.sin(step);
     for(var lon = step; lon < 2*Math.PI; lon += step) {
-      var len = verts.push(xzRadius*Math.sin(lon), radius*Math.cos(lat), xzRadius*Math.cos(lon));
-      indices.push(len-1,len-2,len-verts_per_ring-1);
-      indices.push(len-1,len-verts_per_ring);
+        var len = verts.push(xzRadius*Math.sin(lon), radius*Math.cos(step), xzRadius*Math.cos(lon));
+        indices.push(0, len-2, len-1);
+    }
+    //Fill in hole at boundary
+    indices.push(0, 1, verts_per_ring);
+
+    //All stripes in the middle
+    for(var lat = 2*step; lat < Math.PI-step; lat += step) {
+        var xzRadius = radius*Math.sin(lat);
+        for(var lon = step; lon < 2*Math.PI; lon += step) {
+            var len = verts.push(xzRadius*Math.sin(lon), radius*Math.cos(lat), xzRadius*Math.cos(lon));
+            indices.push(len-1,len-2,len-verts_per_ring-1);
+            indices.push(len-1,len-verts_per_ring);
+        }
+
+        //Fill in hole at boundary
+        indices.push(len-verts_per_ring,len-1,len-2*verts_per_ring);
+        indices.push(len-verts_per_ring,len-2*verts_per_ring,len-2*verts_per_ring+1);
     }
 
+    //Conclude with end cap
+    var end = verts.push(0, radius, 0)-1;
+    var xzRadius = radius*Math.sin(step);
+    for(var lon = step; lon < 2*Math.PI; lon += step) {
+        var len = verts.push(xzRadius*Math.sin(lon), radius*Math.cos(step), xzRadius*Math.cos(lon));
+        indices.push(end, len-2, len-1);
+    }
     //Fill in hole at boundary
-    indices.push(len-verts_per_ring,len-1,len-2*verts_per_ring);
-    indices.push(len-verts_per_ring,len-2*verts_per_ring,len-2*verts_per_ring+1);
-  }
+    indices.push(end, end+1, end+verts_per_ring);
 
-  //Conclude with end cap
-  var end = verts.push(0, radius, 0)-1;
-  var xzRadius = radius*Math.sin(step);
-  for(var lon = step; lon < 2*Math.PI; lon += step) {
-    var len = verts.push(xzRadius*Math.sin(lon), radius*Math.cos(step), xzRadius*Math.cos(lon));
-    indices.push(end, len-2, len-1);
-  }
-  //Fill in hole at boundary
-  indices.push(end, end+1, end+verts_per_ring);
+    var mesh = {};
+    mesh.vertices = new Float32Array(verts);
+    mesh.indices = new Uint16Array(indices);
+    //mesh.vertices = verts;
+    //mesh.indices = indices;
+    return mesh;
+}
 
-  var mesh = {};
-  mesh.vertices = new Float32Array(verts);
-  mesh.indices = new Uint16Array(indices);
-  //mesh.vertices = verts;
-  //mesh.indices = indices;
-  return mesh;
+function computeNormals(mesh) {
+    //for(int i=0; i<mesh.indices.size; i += 3) {
+    //    var tri1 = mesh.vertices[i];
+        //Where is our vector library?
+    //}
 }
--- static/webgl.js
+++ static/webgl.js
@@ -15,14 +15,21 @@
         console.log("done loading shader")
 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+        //gl.clear(gl.COLOR_BUFFER_BIT);
 
-        var mesh = makeSphereMesh(5);
-        renderMesh(gl, mesh, simple);
+        //var mesh = makeSphereMesh(3);
+        var mesh = makeTetrahedronMesh();
+        //var mesh = makeBox(gl);
+        //renderMesh(gl, mesh, simple);
 
-        /*
-        console.log("rendering triangle")
-        renderTriangle(gl, simple);
-        */
+        var angle = 0.0;
+        function animate() {
+            window.requestAnimFrame(animate, canvas);
+            renderMesh(gl, mesh, simple, angle);
+            angle += 1;
+        };
+
+        animate();
     }
     WGL.start = start
 
@@ -38,8 +45,6 @@
     }
     WGL.createWebGLContext = createWebGLContext
 
-    //function loadAssets(gl) {}
-    
     var renderTriangle = function(gl, simple) {
         simple.vertexPositionAttribute = gl.getAttribLocation(simple, "aVertexPosition");
         gl.enableVertexAttribArray(simple.vertexPositionAttribute);
@@ -83,13 +88,19 @@
 
     WGL.renderTriangle = renderTriangle
 
-    var renderMesh = function(gl, mesh, shader) {
+    var renderMesh = function(gl, mesh, shader, angle) {
+        gl.clear(gl.COLOR_BUFFER_BIT, gl.DEPTH_BUFFER_BIT);
+        
         //DIRTY! SETTING MV AND PROJ MATS
         var mvMatrix = mat4.create();
         var pMatrix = mat4.create();
         mat4.perspective(60.0, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
         mat4.identity(mvMatrix);
-        mat4.translate(mvMatrix, [0.0, 0.0, -10.0]);
+        //Set camera
+        mat4.translate(mvMatrix, [0.0, 0.0, -15.0]);
+        //Set model 
+        mat4.rotate(mvMatrix, -Math.PI/16, [1,0,0]);
+        mat4.rotate(mvMatrix, angle * Math.PI / 180.0, [0,1,0]);
 
         //send through the projection and mv matrices
         shader.pMatrixUniform = gl.getUniformLocation(shader, "uPMatrix");
@@ -97,44 +108,34 @@
         gl.uniformMatrix4fv(shader.pMatrixUniform, false, pMatrix);
         gl.uniformMatrix4fv(shader.mvMatrixUniform, false, mvMatrix);
 
-        //enable vertex attributes
+        //Compute normal matrix and send it through
+        var nMatrix = mat4.toInverseMat3(mvMatrix);
+        shader.nMatrix = gl.getUniformLocation(shader, "uNMatrix");
+        gl.uniformMatrix3fv(shader.nMatrix, false, nMatrix);
+
+        //Normal buffer
+        /*
+        var normalBuff = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, normalBuff);
+        gl.bufferData(gl.ARRAY_BUFFER, mesh.normals, gl.STATIC_DRAW);
+        shader.aNormal = gl.getAttribLocation(shader, "aNormal");
+        gl.vertexAttribPointer(shader.aNormal, 3, gl.FLOAT, false, 0, 0);
+        */
+
+        //set up vertex buffer
         shader.vertexPositionAttribute = gl.getAttribLocation(shader, "aVertexPosition");
         gl.enableVertexAttribArray(shader.vertexPositionAttribute);
-        
-        var vertexBuff = gl.createBuffer()
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuff)
-        var vertices = [
-            0.0,  1.0,  0.0,
-            -1.0, -1.0,  0.0,
-            1.0, -1.0,  0.0
-        ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
-        gl.vertexAttribPointer(shader.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0)
 
-        var indexBuff = gl.createBuffer()
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuff);
-        var indices = [0,1,2]
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW)
-
-        gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0);
-
-/*
-        //set up vertex buffer
         var vertexBuff = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuff);
         gl.bufferData(gl.ARRAY_BUFFER, mesh.vertices, gl.STATIC_DRAW);
-        //gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(mesh.vertices), gl.STATIC_DRAW);
         gl.vertexAttribPointer(shader.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
 
         var indexBuff = gl.createBuffer();
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuff);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, mesh.indices, gl.STATIC_DRAW);
-        //gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(mesh.indices), gl.STATIC_DRAW); 
-
-        console.log(mesh)
 
         gl.drawElements(gl.TRIANGLES, mesh.indices.length, gl.UNSIGNED_SHORT, 0);
-*/
     }
 
     WGL.renderMesh = renderMesh
