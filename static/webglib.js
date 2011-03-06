e751dd6c5ceb63115ca7ca1622326b475e4dc00a
Keegan Poppen <keegan.poppen@gmail.com>
2011-03-05 21:52:49 -0800

    new webglib w/ resource loader

--- static/webglib.js
+++ static/webglib.js
@@ -1,122 +1,60 @@
-var MatStack = function(){
-  var stack = []
+(function(){
+    var GLIB = {}
 
-  var _cur = mat4.create()
-  mat4.identity(_cur)
-  stack.push(_cur)
+    var resources;
+    var num_resources;
+    var success_callback;
 
-  var reset_cur = function () {
-    _cur = stack[stack.length-1]
-  }
+    function decrement_resources(){
+        --num_resources
+        console.log('resource loaded! down to ' + num_resources)
+        if(num_resources == 0) {
+            console.log('calling the success handler with object:')
+            console.log(resources)
+            success_callback(resources)
+        }
+    }
 
-  //returns a totally-fragile reference to the current value of the stack
-  this.GetValue = function() {
-    return _cur;
-  }
+    var loadResources = function(path_obj, callback) {
+        resources = { 'textures': {}, 'meshes': {}, 'shaders': {} }
+        num_resources = Object.keys(path_obj.textures).length
+                            + Object.keys(path_obj.meshes).length
+                            + Object.keys(path_obj.shaders).length
 
-  this.Push = function() {
-    var mat = mat4.create(stack[stack.length-1])
-    stack.push(mat)
-    reset_cur()
-  }
+        success_callback = callback
 
-  this.LoadIdentity = function(mat) {
-    mat4.identity(_cur) 
-  }
+        for(texture in path_obj.textures) {
+            (function(){
+                var tex_name = texture
+                var img = new Image()
+                $(img).load(function(){
+                    resources.textures[tex_name] = img
+                    decrement_resources()
+                })
+                img.src = tex_name
+            )()
+        }
+        for(mesh in path_obj.meshes) {
+            (function(){
+                var mesh_name = mesh
+                $.getJSON('/meshes/' + mesh_name, function(data) {
+                    resources.meshes[mesh_name] = data
+                    decrement_resources()
+                }
+            )()
+        }
+        for(shader in path_obj.shaders) {
+            (function(){
+                var shader_name = shader
+                $.get('/shaders/' + shader_name, {}, function(data) {
+                    resources.shaders[shader_name] = data
+                    decrement_resources()
+                }, 'text/plain')
+            })()
+        }
+    }
 
-  this.LoadMatrix = function(mat) {
-    mat4.set(mat, _cur)
-  }
+    GLIB.loadResources = loadResources
 
-  //performs a right multiply. this is negotiable.
-  this.MultMatrix = function(mult) {
-    mat4.multiply(_cur, mult)
-  }
-
-  this.Pop = function() {
-    if(stack.length <= 1) throw "popping off empty stack!"
-
-    stack.pop()//could return / use this value if we wanted...
-    reset_cur()
-  }
-}
-
-//this could even be extended to add listeners for when particular textures are loaded
-//or, at least, when all the textures have loaded
-var TextureDelegate = function(texture_paths) {
-  var tex_map = {}
-
-  for(var i = 0; i < texture_paths.length; ++i) {
-    (function(){
-      var path = texture_paths[i]
-      var i = new Image()
-
-      $(i).load(function(){
-        alert('texture at path ' + path + ' loaded!')
-        tex_map[path].loaded = true
-      })
-
-      i.src = texture_paths[i]
-      tex_map[path] = {'image':i, 'loaded':false}
-    })()
-  }
-
-  this.TextureIsLoaded = function(path) {
-    return tex_map[path] && tex_map[path].loaded
-  }
-
-  this.GetTexFromPath = function(path) {
-    return tex_map[path] && tex_map[path].loaded && tex_map[path].image
-  }
-}
-
-function setupWebGL(canvas, debug) {
-    var gl = WebGLUtils.setupWebGL(canvas);
-    if(debug) gl = WebGLDebugUtils.makeDebugContext(gl);
-
-    gl.viewportWidth = canvas.width;
-    gl.viewportHeight = canvas.height;
-
-    return gl;
-}
-
-function loadShader(gl, sname) {
-    var shaderScript = document.getElementById(sname);
-	if(!shaderScript) return null;
-
-	var shader;
-	if(shaderScript.type === "x-shader/x-fragment") {
-        shader = gl.createShader(gl.FRAGMENT_SHADER);
-    } else if(shaderScript.type === "x-shader/x-vertex") {
-        shader = gl.createShader(gl.VERTEX_SHADER);
-    } else {
-        return null;
-	}
-
-    gl.shaderSource(shader, shaderScript.text);
-    gl.compileShader(shader);
-
-	if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert("Shader did not compile: " + gl.getShaderInfoLog(shader));
-		return null;
-	}
-
-    return shader;
-}
-
-function loadShaderProgram(gl, pname) {
-    var fragmentShader = loadShader(gl, pname + ".frag.glsl");
-	var vertexShader = loadShader(gl, pname + ".vert.glsl");
-	var program = gl.createProgram();
-
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-
-	gl.linkProgram(program);
-
-	if(!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        alert("Shader program fail");
-	}
-
-	return program;
-}
+    return GLIB;
+})()
