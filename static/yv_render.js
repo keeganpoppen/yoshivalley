8c21aa2fb951d1619dca61f89d8e0cf1e35ee2fe
Keegan Poppen <keegan.poppen@gmail.com>
2011-03-10 01:19:26 -0800

    added a bit of code in places to do stuff. like render fuckin' lasers. nbd.

--- static/yv_render.js
+++ static/yv_render.js
@@ -11,17 +11,29 @@ if(!YV || YV === undefined) throw "need to load yv.js first!";
 
     function renderParticles(gl, model) {
         gl.disable(gl.DEPTH_TEST)
+        gl.enable(gl.BLEND)
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE)
+
+        gl.programs.laser.bind()
+
+        var vertex_loc = gl.getAttributeLocation("a_position")
 
         model.particles.lasers.map(function(laser) {
             var start_pos = laser.position.add(laser.velocity.neg)
             var vertices = [start_pos.x, start_pos.y, start_pos.z]
-            var indices = [0]
 
             vertices = new Float32Array(vertices)
-            indices = new Uint16Array(indices)
 
+            var vert_buffer = gl.createBuffer()
+            gl.bindBuffer(gl.ARRAY_BUFFER, vert_buffer)
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW)
+            
+            gl.drawArrays(gl.POINTS, 0, vertices.length)
         })
 
+        gl.programs.laser.unbind()
+
+        gl.disable(gl.BLEND)
         gl.enable(gl.DEPTH_TEST)
     }
 
