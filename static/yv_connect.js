847e82966e3ad23a2f934d64afb72fcaeb7aacd6
Keegan Poppen <keegan.poppen@gmail.com>
2011-03-14 10:25:51 -0700

    now the viewer parses messages from the server correctly

--- static/yv_connect.js
+++ static/yv_connect.js
@@ -3,6 +3,22 @@ if(!YV || YV === undefined) throw "need to load yv.js first!";
 (function(){
     var sock;
 
+    var ONCE = true
+
+    function handleMessagesFromServer(message) {
+        if(message.type !== undefined) {
+            handleMessage(message)
+        } else {
+            var messages = []
+            for(player_id in message) {
+                for(type in message[player_id]) {
+                    messages.push(message[player_id][type])
+                }
+            }
+            messages.map(function(msg){ handleMessage(msg) })
+        }
+    }
+
     function handleMessage(message) {
         if(message.type == 'player:add') {
             YV.AddPlayer(message.player_id, message.color); 
@@ -21,7 +37,7 @@ if(!YV || YV === undefined) throw "need to load yv.js first!";
 
     YV.Connect = function(socket) {
         sock = socket
-        socket.on('message', handleMessage)
+        socket.on('message', handleMessagesFromServer)
         socket.on('disconnect', function(){alert('socket disconnected! just thought you should know');})
 
         //let the server know that we're a viewer so we can get all those pretty little messages
